0c355864a5b2083f3aa94f2911aa18bb4c007723
shuhua shuhua <2125103034@stu.hqu.edu.cn>
2023-08-05 18:19:20 +0800

    8-05

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,17 @@ import Orgnization_details from '../view/userview/Orgnization_details.vue'
 import Position_detailes from '../view/userview/Position_detailes.vue'
 import Examine_page from '../view/userview/Examine_page.vue'
 import All_positions from '../view/userview/All_Positions.vue'
+import TabView from '../view/AdminSys/TabView.vue'
+import jobList from '../view/AdminSys/jobList.vue'
+import jobDetails from '../view/AdminSys/jobDetails.vue'
+import ailApproval from '../view/AdminSys/ailApproval.vue'
+import jobpost from '../view/AdminSys/jobpost.vue'
+import FirstPage from '../view/AdminSys/FirstPage.vue'
+import viewArra from '../view/AdminSys/viewArra.vue'
+import viewList from '../view/AdminSys/viewList.vue'
+import AssessView from '../view/AdminSys/AssessView.vue'
+import AssessRela from '../view/AdminSys/AssessRela.vue'
+import AssessDetail from '../view/AdminSys/AssessDetail.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -21,7 +32,67 @@ const routes = [
   { path: '/Orgnization_details', component: Orgnization_details },
   { path: '/Position_detailes', component: Position_detailes },
   { path: '/Applications', component: Applications },
-  { path: '/All_positions', component: All_positions }
+  { path: '/All_positions', component: All_positions },
+  {
+    path: '/TabView',
+    name: 'TabView',
+    component: TabView, //这里是核心： center作为容器组件，包含左右菜单和右侧的router-view，所以下面的子组件实际是显示在router-view
+     redirect: {name: "FirstPage"},   //输入路由center会重定向到a页面
+    children:
+      [
+        {
+          path: "/FirstPage",
+          name: "FirstPage",
+          component: FirstPage
+        },
+        {
+          path: "/jobList",
+          name: "jobList",
+          component: jobList
+        },
+        {
+          path: "/jobDetails",
+          name: "jobDetails",
+          component: jobDetails
+        },
+        {
+          path: "/jobpost",
+          name: "jobpost",
+          component: jobpost
+        },
+        {
+          path: "/ailApproval",
+          name: "ailApproval",
+          component: ailApproval
+        },
+        {
+          path: "/viewList",
+          name: "viewList",
+          component: viewList
+        },
+        {
+          path: "/viewArra",
+          name: "viewArra",
+          component: viewArra
+        },
+        {
+          path: "/AssessView",
+          name: "AssessView",
+          component: AssessView
+        },
+        {
+          path: "/AssessRela",
+          name: "AssessRela",
+          component: AssessRela
+        },
+        {
+          path: "/AssessDetail",
+          name: "AssessDetail",
+          component: AssessDetail
+        },
+      ]
+  }
+
 ]
 
 const router = new VueRouter({
---
+++ src/view/AdminSys/AssessDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>AssessDetail</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
