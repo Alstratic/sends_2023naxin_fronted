6da7b7a744cf048d1ba04c92a71fffddb471a227
shuhua shuhua <2125103034@stu.hqu.edu.cn>
2023-08-29 09:23:46 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -129,15 +129,18 @@ const router = new VueRouter({
 // 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
 router.beforeEach((to, from, next) => {
   //如果去往登录页则放行 
-    if (to.path === '/') {
+    if (to.path === '/login') {
       next();
     } 
+    else if(to.path === '/LoginSucess'){
+      next();
+    }
     else {
       // 从本地存储里获取token
           let token = localStorage.getItem('token');
           // 判断token是否为空如果为空则跳转到登录页 如果有则放行
           if (token === null || token === '') {
-            next({path:'/'});
+            next({path:'/login'});
           } else {
             next();
           }
